d18ea56189471cae2c24f98e6b706e2a340da4b4
tomaswyz <jackywyz@126.com>
2017-10-11 23:22:02 +0800

    feat(*): init a kotlin android plugin proj

---
+++ src/main/kotlin/com/baidu/fenfayun/demo/Main.kt
@@ -0,0 +1,12 @@
+package com.baidu.fenfayun.demo
+import kotlinx.android.synthetic.main.activity_main.*
+import android.os.Bundle
+import android.app.Activity
+
+class Main: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        
+    }
+}
---
+++ src/test/kotlin/some/SomeTest.kt
@@ -0,0 +1,15 @@
+package some 
+
+import org.junit.Assert
+import org.junit.Test
+
+
+class SomeTest {
+
+    @Test
+    fun firstTest() {
+        Assert.assertTrue(1 == 1)
+    }
+
+}
+
